0fce04b6811ac6dde957f4f2b53ab1c64222df78
Johnrey-18 <johnreylingatong9@gmail.com>
2024-10-03 11:46:13 +0800

    Add files via upload

---
+++ Task2.py
@@ -0,0 +1,7 @@
+num = int(input("How many favorite fruits you would like: "))
+fruits = []
+
+for data in range (num):
+    name = input("enter name of fruits:")
+    fruits.append(name)
+print(fruits)
